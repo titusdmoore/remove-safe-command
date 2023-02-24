dfb24614b28f4bb7dc1434d174e4d4aa5aabc6cd
titusdmoore <titusdmoore@gmail.com>
2023-02-23 20:15:00 -0500

    Began work on refactor

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,8 @@ fn main() {
 // Convert args into usable format
 fn parse_args(raw_args: Args) -> CommandInstance {
     let mut settings: HashMap<String, bool> = HashMap::new();
-    let mut paths: Vec<PathBuf> = Vec::new();
+    // HashMap<PathBuf, IsDeleted (bool)>
+    let mut paths: HashMap<PathBuf, bool> = HashMap::new();
 
     for arg in raw_args {
         if arg.starts_with("-") {
@@ -37,7 +38,7 @@ fn parse_args(raw_args: Args) -> CommandInstance {
             let path_buf: PathBuf = PathBuf::from(arg);
 
             if path_buf.exists() {
-                paths.push(path_buf);
+                paths.insert(path_buf, false);
             } else {
                 println!("Invalid Path: Received {}", path_buf.display());
             }
@@ -59,9 +60,10 @@ fn remove_path(path: &PathBuf, instance: &CommandInstance) {
     // Check if file or dir
     if path.is_dir() {
         if settings.contains_key("recursive") && settings.get("recursive") == Some(&true) {
-            println!("removed {}", path.display());
             match fs::remove_dir_all(path) {
-                Ok(_) => {}
+                Ok(_) => {
+                    instance.dirs_deleted += 1;
+                }
                 Err(e) => {
                     println!("{}", e);
                 }
@@ -71,17 +73,22 @@ fn remove_path(path: &PathBuf, instance: &CommandInstance) {
     } else if path.is_file() {
         // Handle File Delete
         match fs::remove_file(path) {
-          Ok(_) => {}
-          Err(e) => {
-            println!("{}", e);
-          }
+            Ok(_) => {
+                instance.files_deleted += 1;
+            }
+            Err(e) => {
+                println!("{}", e);
+            }
         }
     }
 }
 
+fn command_end(instance: &CommandInstance) {}
+
 struct CommandInstance {
     settings: HashMap<String, bool>,
-    paths: Vec<PathBuf>,
+    // PathBuf, IsRemoved(Bool)
+    paths: HashMap<PathBuf, bool>,
     files_deleted: i32,
     dirs_deleted: i32,
 }
