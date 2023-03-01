b1f6d1ddc03f91ac7afb3b27b5b367126cd99ec6
Titus Moore <tmoore@edgewebware.com>
2023-02-28 23:26:18 -0500

    Began multithreading work

---
+++ readme.md
@@ -0,0 +1,62 @@
+## Badges
+​
+Add badges from somewhere like: [shields.io](https://shields.io/)
+​
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
+[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
+​
+​
+![Logo](https://dev-to-uploads.s3.amazonaws.com/uploads/articles/th5xamgrr6se0x5ro4g6.png)
+​
+​
+# Remove Safe Command
+​
+A drop-in replacement of the `rm` command written in Rust designed to protect users from deleting things unintentially and faster.
+​
+​
+## Usage/Examples
+​
+```bash
+rms -rfq <dir_path> <file_path>...
+```
+​
+​
+## Installation
+​
+Go to releases and download the latest stable release...
+​
+## Screenshots
+​
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+​
+​
+## Roadmap
+​
+- Multi-threading
+​
+- Smarter Delete Protection
+​
+- User Prompt
+​
+- More verbose reporting
+​
+- More granular control of how the command executes
+​
+- Contributor Documentation
+​
+​
+## Feedback
+​
+If you have any feedback, please reach out to us at fake@fake.com
+​
+​
+## Links
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://katherineoelsner.com/)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/titusdmoore/)
+​
+​
+## License
+​
+[MIT](https://choosealicense.com/licenses/mit/)
+​
--- src/instance_path.rs
+++ src/instance_path.rs
@@ -1,4 +1,11 @@
-use std::{path::PathBuf, collections::HashMap, io, fs};
+use crossbeam_channel;
+use std::{
+    collections::HashMap,
+    fs::{self, read_dir},
+    io::{self, Error, ErrorKind},
+    path::PathBuf,
+    thread,
+};
 
 pub struct PathWithStatus {
     pub path: PathBuf,
@@ -7,9 +14,38 @@ pub struct PathWithStatus {
 }
 
 impl PathWithStatus {
-    pub fn remove_path(&mut self, settings: &HashMap<String, bool>) -> Result<&mut PathWithStatus, io::Error> {
+    pub fn remove_path(
+        &mut self,
+        settings: &HashMap<String, bool>,
+    ) -> Result<&mut PathWithStatus, io::Error> {
         if self.path.is_dir() {
             if settings.contains_key("recursive") && settings.get("recursive") == Some(&true) {
+                let child_paths = self.path.get_path_children()?;
+
+                // Check to see if enough to do multi-threaded
+                if child_paths.len() > 4 {
+                    // Create Channel for communicating that the thread has completed the path removal
+                    let (thread_complete_tx_1, thread_complete_rx_1) =
+                        crossbeam_channel::unbounded();
+                    let thread_complete_tx_2 = thread_complete_tx_1.clone();
+                    let thread_complete_tx_3 = thread_complete_tx_1.clone();
+                    let thread_complete_tx_4 = thread_complete_tx_1.clone();
+
+                    // Create Channels for sending paths to threads
+                    let (path_channel_tx_1, path_channel_rx_1) = crossbeam_channel::unbounded();
+                    let (path_channel_tx_2, path_channel_rx_2) = crossbeam_channel::unbounded();
+                    let (path_channel_tx_3, path_channel_rx_3) = crossbeam_channel::unbounded();
+                    let (path_channel_tx_4, path_channel_rx_4) = crossbeam_channel::unbounded();
+
+                    // Create Threads to remove
+                    let thread_handle_1 =
+                        thread::Builder::new()
+                            .name("thread1".to_string())
+                            .spawn(|thread_complete_tx_1, path_channel_rx_1| {
+                                println!("Hello, world!");
+                            });
+                }
+
                 match fs::remove_dir_all(&self.path) {
                     Ok(_) => {
                         self.removed = true;
@@ -42,5 +78,30 @@ impl PathWithStatus {
 
 pub enum PathType {
     File,
-    Dir
+    Dir,
+}
+
+impl WithPathChildren for PathBuf {
+    fn get_path_children(&self) -> Result<Vec<PathBuf>, Error> {
+        if self.is_dir() {
+            let mut paths: Vec<PathBuf> = Vec::new();
+
+            for child_read_dir in self.read_dir()? {
+                let child_read_dir = child_read_dir?;
+
+                paths.push(child_read_dir.path().to_path_buf());
+            }
+
+            return Ok(paths);
+        }
+
+        return Err(Error::new(
+            ErrorKind::Other,
+            "Path provided is not a directory",
+        ));
+    }
+}
+
+trait WithPathChildren {
+    fn get_path_children(&self) -> Result<Vec<PathBuf>, Error>;
 }
